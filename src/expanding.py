1c507ee24b64b5cf695eb78eea7f7199d574effb
Jakub <ksowian@gmail.com>
2024-05-03 10:52:20 +0200

    importing necessary files

---
+++ src/expanding.py
@@ -0,0 +1,7 @@
+import stem_volume
+from stem_volume import *
+import pandas as pd
+import numpy as np
+input_df = pd.read_csv('C:/Users/ksowi/Desktop/FIT/Advanced programming/test/trees_sampled.csv')
+from mapping import species_mapping
+metadata = pd.read_csv('C:/Users/ksowi/Desktop/FIT/Advanced programming/test/form_metadata.csv',sep= ';')
